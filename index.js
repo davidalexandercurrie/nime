7f1a707849a574a85de8e860f6790b0f6624fd1f
David Currie <davidalexandercurrie@gmail.com>
2021-10-25 00:00:42 -0400

    add max stuff

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-let http = require("http");
-let express = require("express");
+let http = require('http');
+let express = require('express');
 let PORT = 3000;
-
+const maxApi = require('max-api');
 let app = express();
 let server = http.createServer(app).listen(PORT);
 let io = require('socket.io')(server);
 
-app.use(express.static("public"));
-console.log("Ready");
+app.use(express.static('public'));
+console.log('Ready');
 
-io.on("connection", (socket) => {
-    console.log(socket.id + " has connected.");
+io.on('connection', socket => {
+  console.log(socket.id + ' has connected.');
 
-    socket.on("makeSound", (data) => {
-        console.log(data);
-    });
-    
-    socket.on("playDrum1", () => {
-        console.log("Drum!");
-    });
+  socket.on('makeSound', data => {
+    console.log(data);
+    maxApi.post(data);
+  });
 
-    socket.on("playDrum2", () => {
-        console.log("Drum!");
-    });
+  socket.on('playDrum1', () => {
+    console.log('Drum!');
+    maxApi.post('Drum1');
+  });
+
+  socket.on('playDrum2', () => {
+    console.log('Drum!');
+    maxApi.post('Drum2');
+  });
 });
